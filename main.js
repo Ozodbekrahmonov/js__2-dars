35a9536f1263a6f9b2a3c8affad897a459ba856d
Ozodbek <ozodrahmonov2006@gmail.com>
2023-05-18 17:48:02 +0500

    js 2-dars
    Bu js 2-dars

---
+++ main.js
@@ -0,0 +1,42 @@
+let a = 10;
+let b = 19;
+let c = 46;
+let d = 49;
+
+/*if (a == 10 || b == 19 || c == 46 || d == 49){
+    document.write("Shartlarni hammasi bajarildi!")
+}
+else{
+    document.write("Bajarilmasi")
+}*/
+
+/*if (a == 11 || b == 18 || c == 48 || d == 51){
+    document.write("Shartlarni hammasi bajarildi!")
+}
+else{
+    document.write("Bajarilmadi!")
+}
+
+if (a == 51 && b == 88 && c == 48 && d == 60){
+    document.write("Shartlarni hammasi bajarildi!")
+}
+else{
+    document.write("Bajarilmadi!")
+}*/
+
+/*  JS data types: underfined,bootlean,object */
+
+/*let soz;*/
+
+/*document.write(typeof soz)*/
+
+/*let rost = true;
+document.write(typeof rost);*/
+
+/*let rost = 150 == 145 ? true:false;
+
+document.write(rost)*/
+
+let rost = 150 == 145 ? true:false;
+
+document.write(rost)
